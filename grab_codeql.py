95bc4f69a2b877d5d532b39ca238f937609a16c4
aegilops <41705651+aegilops@users.noreply.github.com>
2022-06-20 18:20:34 +0100

    Working download for CLI binary zip

--- grab_codeql.py
+++ grab_codeql.py
@@ -9,44 +9,172 @@ Written with 💖 and 🐍 by @aegilops, Field Security Services, GitHub Advance
 from argparse import ArgumentParser, Namespace
 import json
 import logging
-from pathlib import Path
-from urllib.parse import urljoin, urlparse
+from re import M
+from typing import Any, Dict, List, Optional
+from urllib.parse import urljoin
 import requests
+from requests import JSONDecodeError, Session
+from dateutil.parser import isoparse
+from tqdm import tqdm
 
 
 DESCRIPTION="Grab, update and optionally package CodeQL binaries and libraries"
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-GITHUB_API_URI = "https://api.github.com/"
+
+PLATFORM_TO_ASSET_NAME = {
+    "macos": "osx64",
+    "windows": "win64",
+    "linux": "linux64"
+}
+
+
+class QLApi():
+    """API for accessing details about CodeQL CLI binary repo on GitHub."""
+    def __init__(self):
+        self._api_base = "https://api.github.com/"
+        self._owner = "github"
+        self._repo = "codeql-cli-binaries"
+        self._base_uri = urljoin(self._api_base, f"repos/{self._owner}/{self._repo}/")
+
+        self._headers = {
+            "Accept": "application/vnd.github.v3+json"
+        }
+
+        self._session = requests.session()
+
+    def get(self, endpoint: str) -> Optional[List[Dict]]:
+        """Custom getting for GH API."""
+        uri = urljoin(self._base_uri, endpoint)
+        req = requests.Request("GET", uri, headers=self._headers)
+        prep = req.prepare()
+        response = self._session.send(prep)
+        if not response.ok:
+            LOG.error("Response not OK getting releases list")
+            return
+        try:
+            data = response.json()
+        except JSONDecodeError as err:
+            LOG.error("JSON error: %s", err)
+            return None
+        return data
+
+    def tags(self, _tags=[], force=False) -> List[str]:
+        """Get tag names for CLI repo."""
+        if len(_tags) == 0 or force:
+            _tags = [tag["name"] for tag in self.get("tags") if "name" in tag]
+        return _tags
+
+    def releases(self, _releases=[], force=False) -> List[Dict]:
+        """Get full release metadata for CLI repo releases."""
+        if len(_releases) == 0 or force:
+            _releases = self.get("releases")
+        return _releases
+
+    def release(self, tag) -> Optional[Dict]:
+        """Get release metadata by tag."""
+        if tag is None:
+            return self.latest()
+
+        # check if the tag asked for is available
+        if tag not in self.tags():
+            LOG.error("Tag %s not in available list", tag)
+            return None
+        # grab release metadata for the tag
+        try:
+            return next((item for item in self.releases() if item["tag_name"] == tag))
+        except StopIteration:
+            LOG.error("No matching tag in releases.")
+            return None
+
+    def latest(self) -> Optional[Dict]:
+        """Give most recently created release."""
+        try:
+            return max(self.releases(), key=lambda item: isoparse(item["created_at"]))
+        except (ValueError, KeyError) as err:
+            LOG.error("Failed to get latest item")
+            return None
+
+
+def choose_asset(assets, platform) -> Optional[Dict]:
+    """Pick asset from list by selected platform."""
+    name_to_get = f'codeql{"" if platform is None else f"-{PLATFORM_TO_ASSET_NAME.get(platform)}"}.zip'
+    try:
+        return next((asset for asset in assets if asset["name"] == name_to_get))
+    except (StopIteration, KeyError) as err:
+        LOG.error("Failed to match asset to name: %s", err)
+        return None
+
+
+def get_asset(asset: Dict) -> bool:
+    """Grab an asset based on the metadata."""
+    headers = {
+        "Accept": asset["content_type"]
+    }
+
+    size = asset["size"]
+    uri = asset["browser_download_url"]
+    name = asset["name"]
+
+    session = Session()
+    req = requests.Request("GET", uri, headers=headers)
+    prep = req.prepare()
+    response = session.send(prep, stream=True)
+    if not response.ok:
+        LOG.error("Response not OK getting release binary: %s", name)
+        return False
+
+    try:
+        total_length = int(response.headers.get('content-length'))
+    except ValueError:
+        LOG.error("Malformed content-length header")
+        total_length = None
+
+    if total_length is not None and total_length != size:
+        LOG.error("Asset size is not as expected from metadata: expect was %s vs %s", size, total_length)
+        return False
+
+    total_length = size
+
+    with open(name, "wb") as downloaded_asset, tqdm(
+        desc=name,
+        total=total_length,
+        unit='B',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as pb:
+        for data in response.iter_content(chunk_size=4096):
+            downloaded_asset.write(data)
+            pb.update(len(data))
 
 
 def run(args: Namespace) -> None:
     """Main function."""
-    gh_api_cli_releases_uri = urljoin(GITHUB_API_URI, "repos/github/codeql-cli-binaries/releases")
-    headers = {
-        "Accept": "application/vnd.github.v3+json"
-    }
-    session = requests.session()
-    req = requests.Request("GET", gh_api_cli_releases_uri, headers=headers)
-    prep = req.prepare()
-    response = session.send(prep)
-    if response.ok:
-        try:
-            data = response.json()
-        except requests.JSONDecodeError as err:
-            LOG.error("JSON error: %s", err)
-            return
-        LOG.debug(json.dumps(data, indent=2))
-        LOG.debug([res["tag_name"] for res in data if "tag_name" in res])
 
-    else:
-        LOG.error("Response not OK getting releases list")
+    api = QLApi()
+    item = api.release(args.tag)
+
+    if item is None:
+        LOG.error("Error getting release: %s", "latest" if args.tag is None else args.tag)
+        return
+
+    LOG.debug(json.dumps(item, indent=2))
+
+    assets = item["assets"]
+
+    LOG.debug(json.dumps(assets, indent=2))
+
+    asset = choose_asset(assets, args.platform)
+
+    get_asset(asset)
 
 
 def add_arguments(parser: ArgumentParser) -> None:
     """Add arguments to argument parser."""
     parser.add_argument("-d", "--debug", action="store_true", help="Debug output on")
+    parser.add_argument("-t", "--tag", required=False, help="Which tag of the GitHub CLI/library to retrieve (gets 'latest' if absent)")
+    parser.add_argument("-p", "--platform", required=False, choices=["macos", "windows", "linux"], help="Which operating system platform? All are 64 bit")
 
 
 def main() -> None:
