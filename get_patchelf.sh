7b398ae2e1112a6a6feeaf1df281bc5c3ec23da4
aegilops <41705651+aegilops@users.noreply.github.com>
2022-08-19 19:02:01 +0100

    Using `command` instead of `exec`

--- get_patchelf.sh
+++ get_patchelf.sh
@@ -3,16 +3,15 @@
 if uname | grep 'Linux' 2>&1 >/dev/null ; then
     if ! patchelf; then
         echo "Installing patchelf for nuitka to work"
-        if exec brew 2>&1 >/dev/null ; then
+        if command -v brew 2>&1 >/dev/null ; then
             brew install patchelf
-        elif exec apt 2>&1 >/dev/null ; then
-            echo "HERE"
+        elif command -v apt 2>&1 >/dev/null ; then
             sudo apt -y install patchelf
-        elif exec yum 2>&1 >/dev/null ; then
+        elif command -v yum 2>&1 >/dev/null ; then
             sudo yum -y install patchelf
-        elif exec dnf 2>&1 >/dev/null ; then
+        elif command -v dnf 2>&1 >/dev/null ; then
             sudo dnf -y install patchelf
-        elif exec apk 2>&1 >/dev/null ; then
+        elif command -v apk 2>&1 >/dev/null ; then
             sudo apk add patchelf
         fi
     fi
