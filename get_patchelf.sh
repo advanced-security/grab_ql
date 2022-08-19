5c2d19443b08ce0f6dfccff0b8ecffc4332c4de1
aegilops <41705651+aegilops@users.noreply.github.com>
2022-08-19 18:56:12 +0100

    Removed chmod, made executable

--- get_patchelf.sh
+++ get_patchelf.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-if uname | grep 'Linux' ; then
+if uname | grep 'Linux' 2>&1 >/dev/null ; then
     if ! patchelf; then
         echo "Installing patchelf for nuitka to work"
-        if brew 2>&1 >/dev/null ; then
+        if exec brew 2>&1 >/dev/null ; then
             brew install patchelf
-        elif apt 2>&1 >/dev/null ; then
+        elif exec apt 2>&1 >/dev/null ; then
+            echo "HERE"
             sudo apt -y install patchelf
-        elif yum 2>&1 >/dev/null ; then
+        elif exec yum 2>&1 >/dev/null ; then
             sudo yum -y install patchelf
-        elif dnf 2>&1 >/dev/null ; then
+        elif exec dnf 2>&1 >/dev/null ; then
             sudo dnf -y install patchelf
-        elif apk 2>&1 >/dev/null ; then
+        elif exec apk 2>&1 >/dev/null ; then
             sudo apk add patchelf
         fi
     fi
