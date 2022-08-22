3fa8f165e6154d1646b988a0d86975b38f329b26
aegilops <41705651+aegilops@users.noreply.github.com>
2022-08-22 12:48:04 +0100

    Fixed missing closing ]

--- get_nuitka_deps.sh
+++ get_nuitka_deps.sh
@@ -12,37 +12,37 @@ _install_package() {
     if ! command -v "${package}"; then
         echo "Installing ${package}"
         if command -v brew 2>&1 >/dev/null ; then
-            if [ -n "${package_brew}"; then
+            if [ -n "${package_brew}" ]; then
                 brew install "${package_brew}"
             else
                 brew install "${package}"
             fi
         elif command -v nix-env 2>&1 >/dev/null ; then
-            if [ -n "${package_nix}"; then
+            if [ -n "${package_nix}" ]; then
                 nix-env -i "${package_nix}"
             else
                 nix-env -i "${package}"
             fi
         elif command -v apt 2>&1 >/dev/null ; then
-            if [ -n "${package_apt}"; then
+            if [ -n "${package_apt}" ]; then
                 sudo apt -y install "${package_apt}"
             else
                 sudo apt -y install "${package}"
             fi
         elif command -v yum 2>&1 >/dev/null ; then
-            if [ -n "${package_yum}"; then
+            if [ -n "${package_yum}" ]; then
                 sudo yum -y install "${package_yum}"
             else
                 sudo yum -y install "${package}"
             fi
         elif command -v dnf 2>&1 >/dev/null ; then
-            if [ -n "${package_dnf}"; then
+            if [ -n "${package_dnf}" ]; then
                 sudo dnf -y install "${package_dnf}"
             else
                 sudo dnf -y install "${package}"
             fi
         elif command -v apk 2>&1 >/dev/null ; then
-            if [ -n "${package_apk}"; then
+            if [ -n "${package_apk}" ]; then
                 sudo apk add "${package_apk}"
             else
                 sudo apk add "${package}"
